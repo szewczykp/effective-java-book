89edb9f245f8dd5706b5c9491a6847ecbaa81e73
szewczykp <piotrszewczykjava@gmail.com>
2021-05-25 21:33:34 +0200

    practice Builder pattern

---
+++ src/Test.java
@@ -0,0 +1,5 @@
+public class Test {
+    public static void main(String[] args) {
+        NutritionFacts cocaCola = new NutritionFacts.Builder(240, 8).calories(100).sodium(35).carbohydrate(27).build();
+    }
+}
