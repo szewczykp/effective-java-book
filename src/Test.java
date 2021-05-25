82e423a53bdb3d77ade052abcbb5c0540a5f1466
szewczykp <piotrszewczykjava@gmail.com>
2021-05-25 21:55:19 +0200

    builder pattern - hierarchies

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,13 @@
+import hierarchies.NyPizza;
+import hierarchies.Pizza;
+
 public class Test {
     public static void main(String[] args) {
         NutritionFacts cocaCola = new NutritionFacts.Builder(240, 8).calories(100).sodium(35).carbohydrate(27).build();
+
+        NyPizza pizza = new NyPizza.Builder(NyPizza.Size.SMALL)
+                .addTopping(Pizza.Topping.SAUSAGE)
+                .addTopping(Pizza.Topping.ONION)
+                .build();
     }
 }
